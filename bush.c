5a69a2b742e668f3cbbf33096a0dc4435c381d4d
ali019283 <aliburakcetin@protonmail.com>
2022-08-07 12:57:26 +0300

    added pipe

--- bush.c
+++ bush.c
@@ -1,37 +1,82 @@
-#include<stdio.h>
-#include<string.h>
-#include<unistd.h>
-#include<signal.h>
-#include<stdlib.h>
-char *a[128];
+#include <ctype.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *a[512];
+char *ap[512];
 char l[1024];
-void sighand(int no){signal(SIGINT, sighand); fflush(stdout);}
-void sp(char *l){
-  const char s[128] = " ";
-  char *tok;
-  tok = strtok(l,s);
-  for (int i = 0; tok != NULL; i++){
-    a[i] = tok;
-    tok = strtok(NULL, s);
-   }
+int n = -1;
+
+void sighand(int no) {
+    signal(SIGINT, sighand);
+    fflush(stdout);
 }
-int ex(char *l, char **a){
-  if(strcmp(a[0],"exit")==0){exit(0);}
-  pid_t pid = fork();
-  if (!pid){
-    if (execvp(a[0], a) == -1){
-      puts("cant find command");
-      exit(0);
+
+void sp(const char s[2], char **okl, char *okk) {
+    char *tok;
+    tok = strtok(okk, s);
+    for (int i = 0; tok != NULL; i++) {
+        okl[i] = tok;
+        tok = strtok(NULL, s);
     }
-  }
-  return 0;
 }
-int main(void){
-  signal(SIGINT, sighand);
-  while(1){
-    if(!fgets(l,1024,stdin)) return 0;
-    l[strlen(l)-1] = 0;
-    sp(l);
-    ex(l, a);
-  }
+
+int ex(int w, int f, int la, char *z) {
+    n++;
+    memset(a, 0, 512);
+    if (z != NULL)
+        sp(" ", a, ap[n]);
+    else
+        sp(" ", a, l);
+    int g[2];
+    pipe(g);
+    if (strcmp(a[0], "exit") == 0) {
+        exit(0);
+    }
+    pid_t pid = fork();
+    if (!pid) {
+        if (f == 1 && la == 0 && w == 0) {
+            dup2(g[1], STDOUT_FILENO);
+        } else if (f == 0 && la == 0 && w != 0) {
+            dup2(w, STDIN_FILENO);
+            dup2(g[1], STDOUT_FILENO);
+        } else {
+            dup2(w, STDIN_FILENO);
+        }
+        if (execvp(a[0], a) == -1) {
+            puts("cant find command");
+            exit(0);
+        }
+    }
+    if (w)
+        close(w);
+    close(g[1]);
+    if (la)
+        close(g[0]);
+    return g[0];
+}
+
+int main(void) {
+    signal(SIGINT, sighand);
+    while (1) {
+        if (!fgets(l, 1024, stdin))
+            return 0;
+        n = -1;
+        int w = 0, f = 1;
+        char *cm = l;
+        l[strlen(l) - 1] = 0;
+        char *z = strchr(cm, '|');
+        while (z != NULL) {
+            sp("|", ap, cm);
+            *z = '\0';
+            w = ex(w, f, 0, z);
+            cm = z + 1;
+            z = strchr(cm, '|');
+            f = 0;
+        }
+        w = ex(w, f, 1, cm);
+    }
 }
